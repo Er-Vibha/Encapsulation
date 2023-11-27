0aba78d07a5d7fc113508808ae059ed958ab1a3f
Vibha <65108188+Er-Vibha@users.noreply.github.com>
2023-11-27 14:28:11 +0530

    Example Encapsulation

---
+++ com/Details/Student.java
@@ -0,0 +1,10 @@
+package com.Details;
+
+public class Student {
+    public static void main(String[] args) {
+        TestEncap ob=new TestEncap();
+        ob.setName("Vibha");
+        System.out.println(ob.getName());
+    }
+}
+
